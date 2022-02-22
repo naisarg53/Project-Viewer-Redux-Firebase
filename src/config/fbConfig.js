67bbb6d7b605fa756d63bb16b6279d084c5c3065
Naisarg Pandya <59700346+naisarg53@users.noreply.github.com>
2022-02-21 23:45:59 -0500

    Update fbConfig.js

--- src/config/fbConfig.js
+++ src/config/fbConfig.js
@@ -3,13 +3,13 @@ import 'firebase/firestore'
 import 'firebase/auth'
 
 var config = {
-    apiKey: "AIzaSyCdoysW_m0eqnUFhl7CEIchImhuMlvRdKg",
+    apiKey: "Your Api Key",
     authDomain: "fir-crdu.firebaseapp.com",
     databaseURL: "https://fir-crdu.firebaseio.com",
     projectId: "fir-crdu",
     storageBucket: "fir-crdu.appspot.com",
     messagingSenderId: "139202146514",
-    appId: "1:139202146514:web:7e393b970de2f755817d0b"
+    appId: "Your App Id"
 };
 // Initialize Firebase
 firebase.initializeApp(config);
